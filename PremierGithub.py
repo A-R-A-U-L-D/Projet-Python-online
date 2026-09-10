af836e34a27ea0d796b71d9e3cf5157807afbc34
A-R-A-U-L-D <bgarauld18pgmail.com>
2026-09-10 18:33:50 +0200

    Tracé de plusieurs figures

--- PremierGithub.py
+++ PremierGithub.py
@@ -1,11 +1,41 @@
 from turtle import *
-def etoile_5(couleur,taille,angle):
-    color(couleur)
-    e=0
-    while e<5:
-        rotation=180-180/5
-        forward(taille)
-        left(rotation)
-        e+=1
-    
+from math import *
+from random import randrange
+def carre(taille,couleur,angle):
+     color(couleur)
+     c=0
+     right(angle)
+     while c<4:
+          forward(taille)
+          right(90)
+          c+=1
+
+def triangle(taille,couleur,angle):
+     color(couleur)
+     t=0
+     setheading(angle)
+     while t<3:
+          forward(taille)
+          left(120)
+          t+=1
+
+def etoile5(taille,couleur,angle):
+     color(couleur)
+     e=0
+     right(angle)
+     while e<5:
+          forward(taille)
+          right(144)
+          e+=1
+def etoile6(taille,couleur,angle):
+     color(couleur)
+     triangle(taille,couleur,angle)
+     hauteur=(sqrt(3)*taille)/2
+     up()
+     setheading(angle+90)
+     goto(xcor(),ycor()+(2/3)*hauteur)
+     down()
+     triangle(taille,couleur,angle+180)
+
+
 
